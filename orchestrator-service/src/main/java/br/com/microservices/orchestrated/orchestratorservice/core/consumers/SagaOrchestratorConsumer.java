e2514f3807185b6ea2f5b8aeb8ac01bf47978b4b
André Machado <andrels.machado82@gmail.com>
2024-04-06 12:11:28 -0300

    Criação do consumer do ms orchestrator-service

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/consumers/SagaOrchestratorConsumer.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/consumers/SagaOrchestratorConsumer.java
@@ -1,6 +1,6 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.consumers;
 
-import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
+import br.com.microservices.orchestrated.orchestratorservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,11 +15,44 @@ public class SagaOrchestratorConsumer {
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
-            topics = "${spring.kafka.topic.notify-ending}"
+            topics = "${spring.kafka.topic.start-saga}"
     )
 
-    public void consumeNotifyEndingEvent(String payload) {
-        log.info("Receiving ending notification event {} from notify-ending topic", payload);
+    public void consumeStartSagaEvent(String payload) {
+        log.info("Receiving event {} from start-saga topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.orchestrator}"
+    )
+
+    public void consumeOrchestratorEvent(String payload) {
+        log.info("Receiving event {} from orchestrator topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-success}"
+    )
+
+    public void consumeFinishSuccessEvent(String payload) {
+        log.info("Receiving event {} from finish-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-fail}"
+    )
+
+    public void consumeFinishFailEvent(String payload) {
+        log.info("Receiving event {} from finish-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
         log.info(event.toString());
     }
