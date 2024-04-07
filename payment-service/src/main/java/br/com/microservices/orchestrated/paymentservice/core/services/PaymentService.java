4eb55509cf29efe4232c4c535641a80971807696
André Machado <andrels.machado82@gmail.com>
2024-04-07 10:07:59 -0300

    Criação de lógica para um pagamento com SUCESSO

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/services/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/services/PaymentService.java
@@ -2,7 +2,9 @@ package br.com.microservices.orchestrated.paymentservice.core.services;
 
 import br.com.microservices.orchestrated.paymentservice.configs.exceptions.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.dtos.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dtos.History;
 import br.com.microservices.orchestrated.paymentservice.core.dtos.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import br.com.microservices.orchestrated.paymentservice.core.models.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producers.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repositories.PaymentRepository;
@@ -11,6 +13,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus.SUCCESS;
+
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -18,6 +24,8 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MIN_AMOUNT_VALUE = 0.0;
+
     private final JsonUtil jsonUtil;
     private final KafkaProducer kafkaProducer;
     private final PaymentRepository paymentRepository;
@@ -27,6 +35,10 @@ public class PaymentService {
         try {
             checkCurrentValidation(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to make payment: ", ex);
         }
@@ -37,7 +49,7 @@ public class PaymentService {
         Boolean existsTransaction =
                 paymentRepository.existsByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId());
         if (existsTransaction) {
-            throw new ValidationException("There's another transactionID for this validation");
+            throw new ValidationException("There's another transactionID for this validation.");
         }
     }
 
@@ -65,6 +77,39 @@ public class PaymentService {
         return event.getPayload().getProducts().stream()
                 .map(OrderProducts::getQuantity)
                 .reduce(REDUCE_SUM_VALUE.intValue(), Integer::sum);
+    }
+
+    private Payment findByOrderIdAndTransactionId(Event event) {
+        return paymentRepository.findByOrderIdAndTransactionId(
+                event.getPayload().getId(),
+                event.getTransactionId()
+        ).orElseThrow(() -> new ValidationException("Payment not found by orderID and transactionID."));
+    }
+
+    private void validateAmount(double amount) {
+        if (amount < 0.1) throw new ValidationException("The minimum amount available is ".concat(MIN_AMOUNT_VALUE.toString()));
+    }
+
+    private void changePaymentToSuccess(Payment payment) {
+        payment.setStatus(SUCCESS);
+        save(payment);
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
     }
 
     private void save(Payment payment) {
